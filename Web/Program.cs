2919c434cb29d19afda96af15ab096a85b5f4a8f
ent.boris <ent.boris@outlook.fr>
2026-01-27 00:22:15 +0100

    first correct version

--- Web/Program.cs
+++ Web/Program.cs
@@ -1,4 +1,33 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Web.Data;
+using Web.Services;
+
 var builder = WebApplication.CreateBuilder(args);
+
+
+// Configuration de la base de données SQLite
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Configuration d'Identity pour l'authentification admin
+builder.Services.AddDefaultIdentity<IdentityUser>(options =>
+{
+    options.SignIn.RequireConfirmedAccount = false;
+    options.Password.RequireDigit = true;
+    options.Password.RequireLowercase = true;
+    options.Password.RequireUppercase = true;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequiredLength = 6;
+})
+.AddEntityFrameworkStores<ApplicationDbContext>();
+
+builder.Services.Configure<EmailSettings>(
+    builder.Configuration.GetSection("EmailSetting"));
+
+// Enregistrement des services
+builder.Services.AddScoped<IEmailService, EmailService>();
+builder.Services.AddScoped<IRsvpService, RsvpService>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
